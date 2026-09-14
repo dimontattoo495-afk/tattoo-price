49aeaffbe78371fef4c870e08559cc769389af4e
dimontattoo495-afk <dimontattoo495@gmail.com>
2026-09-14 13:30:08 +0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,5 @@
 window.TP_CONFIG = {
-  supabaseUrl: "https://zjncoehvevoxbntysgxv.supabase.co",
-  publishableKey: "sb_publishable_-Z5wMDY6rBgk_FumGc-SrQ_2d3PZD3H",
-  storageBucket: "listing-photos",
-  createListingFunction: "https://zjncoehvevoxbntysgxv.supabase.co/functions/v1/create-listing",
-  tbankCreatePaymentFunction: "https://zjncoehvevoxbntysgxv.supabase.co/functions/v1/tbank-create-payment"
+  supabaseUrl: "https://api.smoke-tattoo.ru",
+  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg5MzcwNTUxLCJleHAiOjE5NDcwNTA1NTF9.b3yB8Ttu1enWRfrZqUzMdVmEQV_0gI2MOhWyVPm76Ik",
+  storageBucket: "listing-photos"
 };
